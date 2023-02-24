53f7f30e9ac931a5c7c27d24dd9d37ef774c08b6
unknown <thatthana.d@gmail.com>
2023-02-24 18:58:50 +0700

    update

---
+++ src/components/Text.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="hello">
+        <div>
+        <h1>ระบบ Encode URL</h1>
+        <p>โดย <a href="https://ballthatthana.com" target="_blank">Ball Thatthana</a> & <a href="https://converrpage.com" target="_blank">ConverrPage Web-Salepage</a></p>
+        <p>วิธีใช้ ใส่ลิงก์ URL ที่ต้องการ encode/ decode ไปในช่อง Iput เเละกด Submit</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: Text
+}
+</script>
