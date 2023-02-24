8ff2414d035d035aac04797747ecba3bf3c04777
unknown <thatthana.d@gmail.com>
2023-02-24 17:25:11 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App Yeah from dev here"/>
+  <HelloWorld msg="Welcome to Your Vue.js App Yeah from Main"/>
 </template>
 
 <script>
