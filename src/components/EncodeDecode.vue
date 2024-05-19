6d71c17648603f8874e7dd6097545a1bc3ff37c3
Thatthana <thatthana.d@gmail.com>
2024-05-19 20:43:16 +0700

    edit type to en

--- src/components/EncodeDecode.vue
+++ src/components/EncodeDecode.vue
@@ -2,15 +2,15 @@
   <div> 
     <div>
       <form class="form-group">
-        <textarea id="url" v-model="url" rows="6" cols="50" class="block p-2.5 w-2/3 sm:w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Url ของคุณ"></textarea>
+        <textarea id="url" v-model="url" rows="6" cols="50" class="block p-2.5 w-2/3 sm:w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Your URL"></textarea>
         <br>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2" @click.prevent="encodeLink">Encode</button>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" @click.prevent="decodeLink">Decode</button>
       </form>
     </div>
     <div class="container">
-      <p class="mt-4">โดย <a href="https://ballthatthana.com" target="_blank" class="text-blue-500 font-bold">Ball Thatthana</a></p>
-      <p class="mt-4">สนับสนุนโดย <a href="https://converrpage.com" target="_blank" class="text-blue-500 font-bold">ConverrPage Web-Salepage</a></p>
+      <p class="mt-4">By <a href="https://ballthatthana.com" target="_blank" class="text-blue-500 font-bold">Ball Thatthana</a></p>
+      <p class="mt-4">Sponsored by <a href="https://converrpage.com" target="_blank" class="text-blue-500 font-bold">ConverrPage Web-Salepage</a></p>
     </div> 
   </div>
 </template>
@@ -25,10 +25,12 @@ export default {
   },
    methods:{
     encodeLink(){
-      this.url = encodeURIComponent(this.url);
+      this.url.length === 0?
+      alert('please type in the url first') : this.url = encodeURIComponent(this.url);
     },
     decodeLink(){
-      this.url = decodeURIComponent(this.url);
+      this.url.length === 0?
+      alert('please type in the url first') : this.url = decodeURIComponent(this.url);
     }
   }
 }
--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="hello">
         <div>
-        <h1 class="font-bold text-xl mb-4">เครื่องมือ Encode/ Decode URL</h1>
-        <p>ใช้สำหรับเเปลงลิงก์ภาษาต่างดาวให้อ่านได้ (Decode) หรือเเปลงสัญลักษณ์ให้อยู่ในรูปเเบบที่ web browser รองรับ (Encode) </p>
-        <p class="font-bold mt-4">วิธีใช้</p>
+        <h1 class="font-bold text-xl mb-4">Encode/ Decode URL Tool</h1>
+        <p>Decode URL easily (That weird URL with special charaters!) and vice versa</p>
+        <p class="font-bold mt-4">How To Use</p>
         <ul class="list-disc mb-4 ml-6">
-            <li>ใส่ลิงก์ไปในช่องว่าง</li>
-            <li>กดปุ่ม Encode หรือ Decode</li>
+            <li>Paste the copied URL in the search box</li>
+            <li>Click Decode or Encode</li>
         </ul>
         </div>
     </div>
